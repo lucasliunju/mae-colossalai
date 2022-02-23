279c0c81915c8143bf4d728c5f52c6671eb91860
“lucasliunju” <lucasliunju@gmail.com>
2022-02-23 16:58:14 +0800

    requirements

--- main_pretrain.py
+++ main_pretrain.py
@@ -196,7 +196,7 @@ def main(args):
     loss_scaler = NativeScaler()
 
     # misc.load_model(args=args, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler)
-`
+
     engine, train_dataloader, test_dataloader, _ = colossalai.initialize(model,
                                                                           optimizer,
                                                                           loss_scaler,
